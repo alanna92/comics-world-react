40d284c731e5bf14d76fd9daf563e46453393b33
Alanna Vanzella <alanna92@gmail.com>
2020-12-02 00:28:09 -0300

    Add react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import React from 'react';
 import GlobalStyle from './styles/global';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Routes from './routes';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <GlobalStyle />
-    </div>
+      <Routes />
+    </Router>
   );
 }
 
